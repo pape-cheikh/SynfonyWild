b35b53f5add55888e9ca628bb723c19895333286
pape_cheikh <dembouche59@gmail.com>
2024-07-29 12:15:11 -0400

    Ajout favicon et mise a jour base html.twig

--- importmap.php
+++ importmap.php
@@ -25,4 +25,21 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    '@picocss/pico' => [
+        'version' => '2.0.6',
+    ],
+    '@picocss/pico/css/pico.min.css' => [
+        'version' => '2.0.6',
+        'type' => 'css',
+    ],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route(path: '/', name: 'app_index')]
-    public function index(): Response
+      public function index(Request $request): Response
+    {
+        dd($request);
+        return new Response('bonjour ' . $request->query->get('name'));
+    }
     {
         return $this->render('Default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
+            'controller_name' => 'DefaultController']);
+        }
 }
